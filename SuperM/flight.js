f54a465d83f4223335ce81ffcd84ba4a35d96f28
AJJackGIS <kafei.aojie2012@foxmail.com>
2018-07-20 17:05:38 +0800

    补充工具类方法,修复了一个Cartesian3静态方法add的调用错误

--- SuperM/flight.js
+++ SuperM/flight.js
@@ -13,7 +13,7 @@ function onload(Cesium) {
     var scene = viewer.scene;
     scene.imageryLayers.removeAll(true);
     scene.globe.baseColor = new Cesium.Color(0.0, 0.0, 0.0, 1.0); // 没有影像时地球的基础颜色，默认为蓝色
-    scene.globe.show = false; // 是否显示地球
+    // scene.globe.show = false; // 是否显示地球
     scene.screenSpaceCameraController.minimumZoomDistance = 20000;
 
     Cesium.loadJson('./flight2.json').then(function (jsonData) {
@@ -23,7 +23,7 @@ function onload(Cesium) {
             var start = makePoint(flight[0]);
             var end = makePoint(flight[1]);
             if (start != undefined && end != undefined) {
-                var positions = addBezier(start, end);
+                var positions = earthMath.getBezierPoints(start, end, 7, 300);
                 viewer.entities.add({ // 用于打底的线
                     polyline: {
                         positions: positions,
@@ -65,62 +65,4 @@ function makePoint(name) {
     var pos = geoCoordMap[name];
     if (pos === undefined) return undefined;
     return [parseFloat(pos[0]), parseFloat(pos[1])];
-}
-
-var EARTH_RADIUS = 6378137.0;    //单位M
-var PI = Math.PI;
-
-function getRad(d) {
-    return d * PI / 180.0;
-}
-
-function getGreatCircleDistance(lat1, lng1, lat2, lng2) {
-    var radLat1 = getRad(lat1);
-    var radLat2 = getRad(lat2);
-
-    var a = radLat1 - radLat2;
-    var b = getRad(lng1) - getRad(lng2);
-
-    var s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2) + Math.cos(radLat1) * Math.cos(radLat2) * Math.pow(Math.sin(b / 2), 2)));
-    s = s * EARTH_RADIUS;
-    s = Math.round(s * 10000) / 10000.0;
-    return s;
-}
-
-function addBezier(pointA, pointB) {
-
-    var origin = [parseFloat(pointA[0]), parseFloat(pointA[1])]; // 第一个点
-    var destination = [parseFloat(pointB[0]), parseFloat(pointB[1])]; // 第二个点
-    var dis = getGreatCircleDistance(origin[1], origin[0], destination[1], destination[0]); // 大圆弧距离
-    var earth = Cesium.Ellipsoid.WGS84; // 椭球体
-    var startPoint = earth.cartographicToCartesian(Cesium.Cartographic.fromDegrees(parseFloat(pointA[0]), parseFloat(pointA[1]), 0.0)); // 经纬度 -> 弧度 -> 大地坐标
-    var endPoint = earth.cartographicToCartesian(Cesium.Cartographic.fromDegrees(parseFloat(pointB[0]), parseFloat(pointB[1]), 0.0));
-    // determine the midpoint (point will be inside the earth)
-    var addCartesian = startPoint.clone();
-    Cesium.Cartesian3.add(startPoint, endPoint, addCartesian); // 坐标和
-    var midpointCartesian = addCartesian.clone();
-    Cesium.Cartesian3.divideByScalar(addCartesian, 2, midpointCartesian); // 除以2  ==========> 求坐标平均值 ===> 中点
-    // move the midpoint to the surface of the earth
-    earth.scaleToGeodeticSurface(midpointCartesian); // 贴到地球表面
-    // add some altitude if you want (1000 km in this case)
-    var midpointCartographic = earth.cartesianToCartographic(midpointCartesian); // 大地 转 弧度
-    midpointCartographic.height = dis / 7;
-    midpointCartesian = earth.cartographicToCartesian(midpointCartographic); // 弧度 转 大地
-    var spline = new Cesium.CatmullRomSpline({
-        times: [0.0, 0.5, 1.0],
-        points: [
-            startPoint,
-            midpointCartesian,
-            endPoint
-        ]
-    });
-    var polylinePoints = [];
-    for (var ii = 0; ii < 30; ++ii) {
-        var evaPoint = spline.evaluate(ii / 30);
-        var cartographic = earth.cartesianToCartographic(evaPoint);
-        // polylinePoints.push([Cesium.Math.toDegrees(cartographic.longitude), Cesium.Math.toDegrees(cartographic.latitude)]);
-        polylinePoints.push(Cesium.Cartesian3.fromDegrees(Cesium.Math.toDegrees(cartographic.longitude), Cesium.Math.toDegrees(cartographic.latitude)));
-    }
-    // console.log(polylinePoints);
-    return polylinePoints;
 }
--- utils/earthMath.js
+++ utils/earthMath.js
@@ -2,18 +2,18 @@
  * 地球工具类
  * @type {{}}
  */
-var earthMath = {
+function EarthMath() {
 
-    EARTH_RADIUS: 6378137.0,    //地球周长(单位M)
+    var EARTH_RADIUS = 6378137.0;    //地球周长(单位M)
 
     /**
      * 角度转弧度
      * @param d
      * @returns {number}
      */
-    getRad: function (d) {
+    this.getRad = function(d) {
         return d * Math.PI / 180.0;
-    },
+    };
 
 
     /**
@@ -24,16 +24,67 @@ var earthMath = {
      * @param lng2 第二个点经度
      * @returns {number}
      */
-    getGreatCircleDistance: function (lat1, lng1, lat2, lng2) {
-        var radLat1 = getRad(lat1);
-        var radLat2 = getRad(lat2);
+    this.getGreatCircleDistance = function(lat1, lng1, lat2, lng2) {
+        var radLat1 = this.getRad(lat1);
+        var radLat2 = this.getRad(lat2);
 
         var a = radLat1 - radLat2;
-        var b = getRad(lng1) - getRad(lng2);
+        var b = this.getRad(lng1) - this.getRad(lng2);
 
         var s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2) + Math.cos(radLat1) * Math.cos(radLat2) * Math.pow(Math.sin(b / 2), 2)));
         s = s * EARTH_RADIUS;
         s = Math.round(s * 10000) / 10000.0;
         return s;
+    };
+
+    /**
+     * 根据控制点获取CatmullRomSpline样条点
+     * @param pointA 第一个点[x,y]
+     * @param pointB 第二个点[x,y]
+     * @param heightR 距离比例
+     * @param num 返回多少个点
+     * @returns {Array}
+     */
+    this.getBezierPoints = function(pointA, pointB, heightR, num) {
+
+        var origin = [parseFloat(pointA[0]), parseFloat(pointA[1])]; // 第一个点
+        var destination = [parseFloat(pointB[0]), parseFloat(pointB[1])]; // 第二个点
+        // 大圆弧距离
+        var dis = this.getGreatCircleDistance(origin[1], origin[0], destination[1], destination[0]);
+        var earth = Cesium.Ellipsoid.WGS84; // 椭球体
+        // 经纬度 -> 弧度 -> 大地坐标
+        var startPoint = earth.cartographicToCartesian(Cesium.Cartographic.fromDegrees(parseFloat(pointA[0]), parseFloat(pointA[1]), 0.0));
+        var endPoint = earth.cartographicToCartesian(Cesium.Cartographic.fromDegrees(parseFloat(pointB[0]), parseFloat(pointB[1]), 0.0));
+        // 计算坐标中点
+        var addCartesian = startPoint.clone(); // 占位
+        Cesium.Cartesian3.add(startPoint, endPoint, addCartesian); // 坐标和
+        var midpointCartesian = addCartesian.clone(); // 占位
+        Cesium.Cartesian3.divideByScalar(addCartesian, 2, midpointCartesian); // 除以2
+        // 拉到贴到地球表面
+        earth.scaleToGeodeticSurface(midpointCartesian, midpointCartesian);
+        // 大地 转 弧度 -- 添加高程
+        var midpointCartographic = earth.cartesianToCartographic(midpointCartesian);
+        // 设置高程
+        midpointCartographic.height = dis / heightR;
+        // 弧度 转 大地
+        midpointCartesian = earth.cartographicToCartesian(midpointCartographic);
+        // 样条插值
+        var spline = new Cesium.CatmullRomSpline({
+            times: [0.0, 0.5, 1.0],
+            points: [
+                startPoint,
+                midpointCartesian,
+                endPoint
+            ]
+        });
+        // 点集集合
+        var polyLinePoints = []; // 大地坐标集合
+        for (var i = 0; i < num; i++) {
+            var evaPoint = spline.evaluate(i / num); // 估算
+            polyLinePoints.push(evaPoint);
+        }
+        return polyLinePoints;
     }
 }
+
+var earthMath = new EarthMath();
